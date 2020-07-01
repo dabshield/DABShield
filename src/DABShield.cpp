3a13c8f896ada5ae9d4e21b54c2942d07a2504f7
dabshield <40293768+dabshield@users.noreply.github.com>
2020-07-01 09:11:30 +0100

    Update DABShield.cpp

--- src/DABShield.cpp
+++ src/DABShield.cpp
@@ -12,6 +12,7 @@
 // v0.10 19/09/2018 - Removed Serial.print
 // v0.11 06/09/2019 - Added FM seek and valid
 // v0.12 17/12/2019 - Corrected DAB Freqs
+// v1.1.3 01/07/2020 - Updated Version Format for Arduino IDE Managed Libraries
 ///////////////////////////////////////////////////////////
 #include "DABShield.h"
 #include "Si468xROM.h"
