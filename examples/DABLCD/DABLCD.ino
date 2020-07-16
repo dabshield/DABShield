e0e9a4210048a84894db9513893cf6fd2f7e394c
DAB Shield <40293768+dabshield@users.noreply.github.com>
2020-07-16 14:46:33 +0100

    Update DABLCD.ino

--- examples/DABLCD/DABLCD.ino
+++ examples/DABLCD/DABLCD.ino
@@ -1,6 +1,14 @@
 ////////////////////////////////////////////////////////////
 // DAB/LCD Shield Example App
 // AVIT Research Ltd
+//
+// Library Dependencies:
+//    * DABShiled
+//    * Adafruit_RGBLCDShiled
+//    * FlashStorage (SAMD devices e.g. M0, Zero etc)
+//    * DueFlashStorage (Ardino Due)
+//
+// Version Info:
 // v0.1 22/02/2018 - initial release
 // v0.2 05/07/2018 - added faster tune
 // v0.3 19/09/2018 - array safeguards
@@ -10,7 +18,10 @@
 // v0.7 20/12/2019 - Ralf's no dab station additions
 // v0.8 06/02/2020 - Fix for no dab stations EEPROM to allow FM.
 // v0.9 05/06/2020 - non volatile memory storeage for Due
+// v1.0 16/07/2020 - Display mapping for EU chars, Interface speed up for next/previous
 ////////////////////////////////////////////////////////////
+#include "Arduino.h"
+
 #include <SPI.h>
 #include <DABShield.h>
 
@@ -167,40 +178,36 @@ int16_t magic_check;
 #endif
 #endif
 
+void ServiceData(void);
+uint8_t DAB_scan(void);
+void process_buttons(uint8_t buttons);
+void process_display(void);
+
+//LCD custom chars for accented vowels.
+const uint8_t abar[] = {0x0E,0x00,0x0E,0x01,0x0F,0x11,0x0F,0x00};  //0x08
+const uint8_t ebar[] = {0x0E,0x00,0x0E,0x11,0x1F,0x10,0x0E,0x00};  //0x09
+const uint8_t ibar[] = {0x0E,0x00,0x0C,0x04,0x04,0x04,0x0E,0x00};  //0x0A
+const uint8_t obar[] = {0x0E,0x00,0x0E,0x11,0x11,0x11,0x0E,0x00};  //0x0B
+const uint8_t ubar[] = {0x0E,0x00,0x11,0x11,0x11,0x13,0x0D,0x00};  //0x0C
+
+//Conversion from extended EBU Latin (DAB/RDS) to LCD charset  (0x80 to 0xFF)
+const char EBULatintoLCD[0x80] = {0x08,0x08,0x09,0x09,0x0A,0x0A,0x0B,0x0B,0x0C,0x0C,'N', 'C', 'S', 0xE2,'i', 'Y',
+                                  0x08,0x08,0x09,0x09,0x0A,0x0A,0x0B,0x0B,0x0C,0x0C,'n', 'c', 's', 'g', 'i', 'y', 
+                                  'a', 'a', 'c', '/', 'G', 'e', 'n', 0x0B,0xF7,'$', '$', '$', 0x7F,'^', 0x7E,'v', 
+                                  '0', '1', '2', '3', '+', 'I', 'n', 0x0C,0xE4,'?', 0xFD,0xDF,'_', '_', '_', '_', 
+                                  'A', 'A', 'E', 'E', 'I', 'I', 'O', 'O', 'U', 'U', 'R', 'C', 'S', 'Z', 'D', 'L', 
+                                  'A', 'A', 'E', 'E', 'I', 'I', 'O', 'O', 'U', 'U', 'r', 'c', 's', 'z', 'd', 'l', 
+                                  'A', 'A', 'E', 'E', 'y', 'Y', 'O', 'O', 'p', 'n', 'R', 'C', 'S', 'Z', 't', 'd', 
+                                  0x08,0x08,'e', 'e', 'w', 'y', 'O', 'o', 'p', 'n', 'r', 'c', 's', 'z', 't', ' '};
 
 void replaceSpecialCharsInplace(char * p, int len)
 {
   for (int j = 0; j < (len); j++)
   {
-    //correct wrong chars
+    //correct wrong chars for our LCD display
     unsigned char * cc = (unsigned char*) &p[j];
-    switch (*cc)
-    {
-      case 'ä': //0xE4
-        *cc = 0xE1;
-        break;  //ä ->OK
-      case 'ö': //0xF6
-        *cc = 0xEF;
-        break;  //ö ->OK
-      case 'ü': //0xFC
-        *cc = 0xF5;
-        break;  //ü ->OK
-      case 'Ä': //0xC4
-        *cc = 0xE1;
-        break;  //Ä ->OK
-      case 'Ö': //0xD6
-        *cc = 0xEF;
-        break;  //Ö ->OK
-      case 'Ü': //0xDC
-        *cc = 0xF5;
-        break;  //Ü ->OK
-      case 'ß': //0xDF
-        *cc = 0xE2;
-        break;  //ß ->OK
-      default:
-        //*cc |= 0x80;
-        break;
-    }
+    if(*cc > 0x7F)
+      *cc = EBULatintoLCD[*cc - 0x80];
   }
 }
 
@@ -226,10 +233,17 @@ void setup() {
   lcd.begin(16, 2);
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("AVIT DAB/FM v0.8");
+  lcd.print("AVIT DAB/FM v1.0");
   lcd.setCursor(0, 1);
   lcd.print("Initialising....");
   lcd.setBacklight(1);
+  
+  lcd.createChar(0, (uint8_t *)abar);
+  lcd.createChar(1, (uint8_t *)ebar);
+  lcd.createChar(2, (uint8_t *)ibar);
+  lcd.createChar(3, (uint8_t *)obar);
+  lcd.createChar(4, (uint8_t *)ubar);
+  
   display_state = DISPLAY_INIT;
 
   //DAB Setup
@@ -263,55 +277,9 @@ void setup() {
         ((dab_mode == true) && (NumOfEnsembles == 0)) )
     {
       dab_mode = true;
-      ensemble = 0;
-      service = 0;
       vol = 25;
       Dab.vol(vol);
-
-      //SCAN for Services...
-      lcd.setCursor(0, 1);
-      lcd.print("Scanning...     ");
-
-      NumOfEnsembles = DAB_scan();
-
-      //Write to EEPROM even if nothing found as EEPROM will be need if we swtich to FM.
-      lcd.setCursor(0, 1);
-      lcd.print("save to EEPROM..");
-
-      magic_check = MAGIC;
-      
-      #ifdef DUE_FLASH
-        dueFlashStorage.write(FLASH_NumOfEnsembles, NumOfEnsembles);
-        dueFlashStorage.write(FLASH_ensemble, ensemble);
-        dueFlashStorage.write(FLASH_service, service);
-        dueFlashStorage.write(FLASH_dab_mode, dab_mode);
-        dueFlashStorage.write(FLASH_vol, vol);
-        dueFlashStorage.write(FLASH_magic_check, (uint8_t *)&magic_check, sizeof(magic_check));
-        dueFlashStorage.write(FLASH_fm_freq, (uint8_t *)&fm_freq, sizeof(fm_freq));
-        dueFlashStorage.write(FLASH_Ensembles, (uint8_t *)&Ensemble[0], sizeof(Ensemble));        
-      #else
-        magic_check_storage.write(magic_check);
-        NumOfEnsembles_storage.write(NumOfEnsembles);
-        Ensemble_storage.write(*pEnsemples);
-        LastPlayedEnsemble_storage.write(ensemble);
-        LastPlayedService_storage.write(service);
-        DabMode_storage.write(dab_mode);
-        LastVolume_storage.write(vol);
-        FMFreq_storage.write(fm_freq);
-      #endif
-
-      if (NumOfEnsembles > 0)
-      {
-        //Something has been found so tune to First service    
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Tuning ensemble:");
-        lcd.setCursor(0, 1);
-        lcd.print(Ensemble[ensemble].Ensemble);
-
-        Dab.tune(Ensemble[ensemble].freq_index);
-        Dab.set_service(service);
-      }
+      ScanforServices();
     }
     else
     {
@@ -358,53 +326,89 @@ void setup() {
       }
     }
 #else
-    //SCAN for Services...
-    lcd.setCursor(0, 1);
-    lcd.print("Scanning...     ");
-
-    NumOfEnsembles = DAB_scan();
-    if (NumOfEnsembles > 0)
-    {
-      //Something has been found so tune to First service
-      ensemble = 0;
-      service = 0;
-
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Tuning ensemble:");
-      lcd.setCursor(0, 1);
-      lcd.print(Ensemble[ensemble].Ensemble);
-
-      Dab.tuneservice(Ensemble[ensemble].freq_index, Ensemble[ensemble].service[service].ServiceID, Ensemble[ensemble].service[service].CompID);
-    }
+    ScanforServices();
 #endif
   }
   previous = millis();
 }
 
+void ScanforServices(void)
+{
+  //SCAN for Services...
+  lcd.setCursor(0, 1);
+  lcd.print("Scanning...     ");
+
+  ensemble = 0;
+  service = 0;
+  NumOfEnsembles = DAB_scan();
+
+#ifdef USE_EEPROM
+  //Write to EEPROM even if nothing found as EEPROM will be need if we swtich to FM.
+  lcd.setCursor(0, 1);
+  lcd.print("save to EEPROM..");
+
+  magic_check = MAGIC; 
+  #ifdef DUE_FLASH
+    dueFlashStorage.write(FLASH_NumOfEnsembles, NumOfEnsembles);
+    dueFlashStorage.write(FLASH_ensemble, ensemble);
+    dueFlashStorage.write(FLASH_service, service);
+    dueFlashStorage.write(FLASH_dab_mode, dab_mode);
+    dueFlashStorage.write(FLASH_vol, vol);
+    dueFlashStorage.write(FLASH_magic_check, (uint8_t *)&magic_check, sizeof(magic_check));
+    dueFlashStorage.write(FLASH_fm_freq, (uint8_t *)&fm_freq, sizeof(fm_freq));
+    dueFlashStorage.write(FLASH_Ensembles, (uint8_t *)&Ensemble[0], sizeof(Ensemble));        
+  #else
+    magic_check_storage.write(magic_check);
+    NumOfEnsembles_storage.write(NumOfEnsembles);
+    Ensemble_storage.write(*pEnsemples);
+    LastPlayedEnsemble_storage.write(ensemble);
+    LastPlayedService_storage.write(service);
+    DabMode_storage.write(dab_mode);
+    LastVolume_storage.write(vol);
+    FMFreq_storage.write(fm_freq);
+  #endif
+#endif
+
+  if (NumOfEnsembles > 0)
+  {
+    //Something has been found so tune to First service    
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Tuning ensemble:");
+    lcd.setCursor(0, 1);
+    lcd.print(Ensemble[ensemble].Ensemble);
+
+    Dab.tune(Ensemble[ensemble].freq_index);
+    Dab.set_service(service);
+  }
+}
+
 //Callback from DAB Library when new service data (radio text) is recieved.
 void ServiceData(void)
 {
-  //new text...but we compare to the previous text, as an update isn't always different next.
-  if (strcmp(service_text, Dab.ServiceData) != 0)
+  //remove spaces from end of string.
+  for (uint8_t i = strlen(Dab.ServiceData) - 1; i > 0; i--)
   {
+    if (Dab.ServiceData[i] == ' ')
+    {
+      Dab.ServiceData[i] = '\0';
+    }
+    else
+    {
+      break;
+    }
+  }
+  //convert charset for our LCD display
+  replaceSpecialCharsInplace(Dab.ServiceData, strlen(Dab.ServiceData));         
+    
+  //new text...but we compare to the previous text, as an update isn't always different.
+  if (strcmp(service_text, Dab.ServiceData) != 0)
+  {   
     //Text has changed, copy to current text and reset scrolling to beginning
     strcpy(service_text, Dab.ServiceData);
-    replaceSpecialCharsInplace(service_text, strlen(service_text));
-    //remove spaces from end of string.
-    for (uint8_t i = strlen(service_text) - 1; i > 0; i--)
-    {
-      if (service_text[i] == ' ')
-      {
-        service_text[i] = '\0';
-      }
-      else
-      {
-        break;
-      }
-    }
     text_index = 0;
   }
+  replaceSpecialCharsInplace(Dab.ps, strlen(Dab.ps));
   if (strcmp(fm_ps, (char*)Dab.ps) != 0)
   {
     strcpy(fm_ps, (char*)Dab.ps);
@@ -469,11 +473,7 @@ void loop() {
     if (buttons == 0)
     {
       debouncebuttonsTimer = 0;
-      if (lastbuttons != 0)
-      {
-        //release
-        process_buttons(buttons);
-      }
+      process_buttons(buttons);
     }
     lastbuttons = buttons;
     process_display();
@@ -518,6 +518,7 @@ void process_buttons(uint8_t buttons)
     {
       if (dab_mode == true)
       {
+        seek_timer = 5;
         DAB_PreviousService();
         display_state = DISPLAY_SERVICE;
       }
@@ -549,6 +550,7 @@ void process_buttons(uint8_t buttons)
     if (buttons & BUTTON_RIGHT) {
       if (dab_mode == true)
       {
+        seek_timer = 5;
         DAB_NextService();
         display_state = DISPLAY_SERVICE;
       }
@@ -599,40 +601,58 @@ void process_buttons(uint8_t buttons)
       //Seek Release
       if (seek_timer > 0)
       {
-        if (seek_timer < 5)
+        if (dab_mode == true)
         {
-          if (seek_dir == 1)
-          {
-            if (fm_freq < 10800)
+            if(seek_timer)
             {
-              fm_freq += 10;
+              seek_timer--;
+              if(seek_timer == 0)
+              {
+                lcd.setCursor(0, 1);
+                lcd.print("tuning...       ");
+                Dab.tuneservice(Ensemble[ensemble].freq_index, Ensemble[ensemble].service[service].ServiceID, Ensemble[ensemble].service[service].CompID);  
+                display_state = DISPLAY_DELAY;
+                displayTimer = 1000;     
+              }
             }
-            else
-            {
-              fm_freq = 8750;
-            }
-          }
-          else
-          {
-            if (fm_freq > 8750)
-            {
-              fm_freq -= 10;
-            }
-            else
-            {
-              fm_freq = 10800;
-            }
-          }
-          Dab.tune(fm_freq);
-#ifdef USE_EEPROM
-          writeCurrrentSettingsToFlash = true;
-          writeCurrentSettingsToFlashDelayTimer = WAIT_SAVECURRENTSETTINGS; //prevent writing while selecting
-#endif
-          service_text[0] = '\0';
-          fm_ps[0] = '\0';
-          display_state = DISPLAY_SERVICE;
         }
-        seek_timer = 0;
+        else
+        {        
+          if (seek_timer < 5)
+          {
+            if (seek_dir == 1)
+            {
+              if (fm_freq < 10800)
+              {
+                fm_freq += 10;
+              }
+              else
+              {
+                fm_freq = 8750;
+              }
+            }
+            else
+            {
+              if (fm_freq > 8750)
+              {
+                fm_freq -= 10;
+              }
+              else
+              {
+                fm_freq = 10800;
+              }
+            }
+            Dab.tune(fm_freq);
+  #ifdef USE_EEPROM
+            writeCurrrentSettingsToFlash = true;
+            writeCurrentSettingsToFlashDelayTimer = WAIT_SAVECURRENTSETTINGS; //prevent writing while selecting
+  #endif
+            service_text[0] = '\0';
+            fm_ps[0] = '\0';
+            display_state = DISPLAY_SERVICE;
+          }
+          seek_timer = 0;
+        }        
       }
       //Select Release
       else if (button_select_timer > 0)
@@ -677,38 +697,82 @@ void process_buttons(uint8_t buttons)
   }
   else //menu
   {
-    menu_timer = 2000;  //stay in menu mode
-    if (buttons & BUTTON_RIGHT)
+    if (dab_mode == true)
     {
-      if (menu_mode < 1)
+      if (buttons & BUTTON_RIGHT)
       {
-        menu_mode++;
+        menu_timer = 5000;  //stay in menu mode
+        if (menu_mode < 2)
+        {
+          menu_mode++;
+        }
       }
-    }
-    if (buttons & BUTTON_LEFT)
-    {
-      if (menu_mode > 1)
+      if (buttons & BUTTON_LEFT)
       {
-        menu_mode--;
-      }
-    }
-    if (menu_mode == 1) //FM
-    {
-      if (buttons & BUTTON_SELECT)
+        menu_timer = 5000;  //stay in menu mode
+        if (menu_mode > 1)
+        {
+          menu_mode--;
+        }
+      }      
+      if (menu_mode == 1) //FM
       {
-        if (dab_mode == true)
+        if (buttons & BUTTON_SELECT)
         {
           Dab.begin(1);
           Dab.tune(fm_freq);
-          Dab.vol(vol);
 #ifdef USE_EEPROM
           writeCurrrentSettingsToFlash = true;
           writeCurrentSettingsToFlashDelayTimer = WAIT_SAVECURRENTSETTINGS; //prevent writing while selecting
 #endif
           text_mode = TEXTMODE_SERVICEDATA;
           dab_mode = false;
+          Dab.vol(vol);
+          service_text[0] = '\0';
+          fm_ps[0] = '\0';
+          menu_mode = 0;
+          menu_timer = 0;
+          display_state = DISPLAY_DELAY;
+          displayTimer = 0;
         }
-        else
+      }
+      else if (menu_mode == 2) //SCAN
+      {
+        if (buttons & BUTTON_SELECT)
+        {
+          lcd.setCursor(0, 0);
+          lcd.print("Re-SCAN         ");          
+          ScanforServices();        
+          service_text[0] = '\0';
+          fm_ps[0] = '\0';
+          menu_mode = 0;
+          menu_timer = 0;
+          display_state = DISPLAY_DELAY;
+          displayTimer = 0;   
+        }     
+      }
+    }
+    else
+    {
+      if (buttons & BUTTON_RIGHT)
+      {
+        menu_timer = 5000;  //stay in menu mode
+        if (menu_mode < 1)
+        {
+          menu_mode++;
+        }
+      }
+      if (buttons & BUTTON_LEFT)
+      {
+        menu_timer = 5000;  //stay in menu mode
+        if (menu_mode > 1)
+        {
+          menu_mode--;
+        }
+      }
+      if (menu_mode == 1) //DAB
+      {
+        if (buttons & BUTTON_SELECT)
         {
           Dab.begin(0);
           if (NumOfEnsembles > 0)
@@ -727,14 +791,14 @@ void process_buttons(uint8_t buttons)
             lcd.print("No DAB stations ");
           }
           dab_mode = true;
+          Dab.vol(vol);        
+          service_text[0] = '\0';
+          fm_ps[0] = '\0';
+          menu_mode = 0;
+          menu_timer = 0;
+          display_state = DISPLAY_DELAY;
+          displayTimer = 0;
         }
-        Dab.vol(vol);
-        service_text[0] = '\0';
-        fm_ps[0] = '\0';
-        menu_mode = 0;
-        menu_timer = 0;
-        display_state = DISPLAY_DELAY;
-        displayTimer = 0;
       }
     }
   }
@@ -871,6 +935,7 @@ void process_display(void)
             }
             else
             {
+              lcd.print("                ");
               displayTimer = 1000;
             }
           }
@@ -949,24 +1014,23 @@ void process_display(void)
       lcd.setCursor(0, 0);
       lcd.print("Menu            ");
       lcd.setCursor(0, 1);
-      if (menu_mode <= 1)
-      {
-        if (dab_mode == true)
+      if (dab_mode == true)
+      {     
+        if (menu_mode <= 1)
         {
           lcd.print("FM Mode         ");
         }
-        else
+        else if (menu_mode == 2)
+        {
+          lcd.print("Re-SCAN         ");
+        }
+      }
+      else
+      {
+        if (menu_mode <= 1)
         {
           lcd.print("DAB Mode        ");
         }
-      }
-      else if (menu_mode == 2)
-      {
-        lcd.print("Item2           ");
-      }
-      else if (menu_mode == 3)
-      {
-        lcd.print("Item3           ");
       }
       break;
   }
@@ -1029,6 +1093,8 @@ uint8_t DAB_scan(void)
     {
       //Copy Services into Array...
       Ensemble[ensemble_index].freq_index = freq_index;
+      //Convert charset for our LCD display
+      replaceSpecialCharsInplace(Dab.Ensemble, strlen(Dab.Ensemble));         
       strcpy(Ensemble[ensemble_index].Ensemble, Dab.Ensemble);
       //ensure we don't overflow our array.
       numofservices = Dab.numberofservices;
@@ -1041,6 +1107,8 @@ uint8_t DAB_scan(void)
       {
         Ensemble[ensemble_index].service[i].ServiceID = Dab.service[i].ServiceID;
         Ensemble[ensemble_index].service[i].CompID = Dab.service[i].CompID;
+        //Convert charset for our LCD display
+        replaceSpecialCharsInplace(Dab.service[i].Label, strlen(Dab.service[i].Label));         
         strcpy(Ensemble[ensemble_index].service[i].Label, Dab.service[i].Label);
       }
       ensemble_index++;
@@ -1069,15 +1137,9 @@ void DAB_NextService(void)
       {
         ensemble = 0;
       }
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Tuning ensemble:");
-      lcd.setCursor(0, 1);
-      lcd.print(Ensemble[ensemble].Ensemble);
       service = 0;
     }
     service_text[0] = '\0';
-    Dab.tuneservice(Ensemble[ensemble].freq_index, Ensemble[ensemble].service[service].ServiceID, Ensemble[ensemble].service[service].CompID);
 #ifdef USE_EEPROM
     writeCurrrentSettingsToFlash = true;
     writeCurrentSettingsToFlashDelayTimer = WAIT_SAVECURRENTSETTINGS; //prevent writing while selecting
@@ -1103,16 +1165,9 @@ void DAB_PreviousService(void)
       {
         ensemble = NumOfEnsembles - 1;
       }
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Tuning ensemble:");
-      lcd.setCursor(0, 1);
-      lcd.print(Ensemble[ensemble].Ensemble);
-
       service = Ensemble[ensemble].numberofservices - 1;
     }
     service_text[0] = '\0';
-    Dab.tuneservice(Ensemble[ensemble].freq_index, Ensemble[ensemble].service[service].ServiceID, Ensemble[ensemble].service[service].CompID);
 #ifdef USE_EEPROM
     writeCurrrentSettingsToFlash = true;
     writeCurrentSettingsToFlashDelayTimer = WAIT_SAVECURRENTSETTINGS; //prevent writing while selecting
